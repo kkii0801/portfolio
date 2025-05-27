7c288a4fbed31e5e672ae0dcfdffb56b3b1aa9e6
kkii0801 <kkii0801@gmail.com>
2025-05-27 10:16:06 +0900

    2025-05-27-04

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ window.addEventListener("load", () => {
 			slidesPerView: 3.5,
 			spaceBetween: 20,
 			slidesOffsetBefore: 10,
-			slidesOffsetAfter: 140,
+			slidesOffsetAfter: 80,
 			breakpoints : {
 				350: {
 					slidesPerView: 2.25,
